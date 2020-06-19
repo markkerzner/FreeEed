1f806e19782fe5074211970f02f12f20bd0cc0f0
Farshid Mehrtash <mehrtash.fm@gmail.com>
2020-06-19 22:26:05 +0430

    adding custodian to files removing path from file

--- freeeed-processing/src/main/java/org/freeeed/Entity/ProjectFile.java
+++ freeeed-processing/src/main/java/org/freeeed/Entity/ProjectFile.java
@@ -1,6 +1,10 @@
 package org.freeeed.Entity;
 
+import org.freeeed.ServiceDao.CustodianService;
+import org.freeeed.util.Util;
+
 import javax.persistence.*;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,26 +22,42 @@ public class ProjectFile {
     private String hash;
 
     @ManyToOne
-    @JoinColumn(name = "pathId")
-    private ProjectPath path;
+    @JoinColumn(name = "custodianId")
+    private ProjectCustodian custodian;
 
     @OneToMany(mappedBy = "file", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<ProjectMetadata> metadataList = new ArrayList<>();
 
-    public ProjectPath getPath() {
-        return path;
+    public ProjectFile() {
+    }
+
+    public ProjectFile(String file, ProjectCustodian custodian) {
+        this.file = file;
+        this.custodian = custodian;
+    }
+
+    public ProjectFile(String file, Project project) {
+        this.file = file;
+        custodian = CustodianService.getInstance().getCustodianByName(Util.getCustodianFromPath(new File(file)), project);
+    }
+
+    public int getFileId() {
+        return fileId;
+    }
+
+    public ProjectCustodian getCustodian() {
+        return custodian;
+    }
+
+    public void setCustodian(ProjectCustodian custodian) {
+        this.custodian = custodian;
     }
 
     public List<ProjectMetadata> getMetadataList() {
         return metadataList;
     }
 
-    public ProjectFile(String file, ProjectPath projectPath) {
-        this.file = file;
-        this.path = projectPath;
-    }
-
-    public String getFile() {
+    public String getFilePath() {
         return file;
     }
 
@@ -47,5 +67,17 @@ public class ProjectFile {
 
     public String getHash() {
         return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public File getFile() {
+        return new File(file);
+    }
+
+    public String getExtension() {
+        return Util.getExtension(this.getFile().getName());
     }
 }
