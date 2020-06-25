15efaefa76189ef883afbd0d697c58136caa723f
Farshid Mehrtash <mehrtash.fm@gmail.com>
2020-06-26 02:16:55 +0430

    removing native copier

--- freeeed-processing/src/main/java/org/freeeed/mr/MetadataWriter.java
+++ freeeed-processing/src/main/java/org/freeeed/mr/MetadataWriter.java
@@ -16,26 +16,17 @@
  */
 package org.freeeed.mr;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.tika.metadata.Metadata;
-import org.freeeed.Entity.Project;
 import org.freeeed.Entity.ProjectFile;
 import org.freeeed.ServiceDao.MetadataService;
-import org.freeeed.main.*;
-import org.freeeed.metadata.ColumnMetadata;
-import org.freeeed.main.DiscoveryFile;
 import org.freeeed.services.ProcessingStats;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.*;
 import java.util.*;
-
+//TODO: In memory cache please.
 public class MetadataWriter {
     private static final Logger LOGGER = LoggerFactory.getLogger(MetadataWriter.class);
     private static volatile MetadataWriter mInstance;
-    private File metadataFile;
-    private String tmpFolder;
 
     private MetadataWriter() {
     }
@@ -65,61 +56,10 @@ public class MetadataWriter {
                 }
             }
             MetadataService.getInstance().newMetaData(name, metadata.get(name), projectFile);
+            ProcessingStats.getInstance().taskIsTika(projectFile.getFile().getName(), name);
         }
         //TODO: Write text to file immediately
         LOGGER.info(projectFile.getFile() + " Done");
         ProcessingStats.getInstance().increaseItemCount(projectFile.getFile().length());
-    }
-
-    public void packNative() {
-/*
-        ProcessingStats.getInstance().taskIsNative();
-        int indexNativeLink = 0, indexStageFile = 0, indexExceptionLink = 0;
-        boolean checkingHeader = true;
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(metadataFile));
-            String line = reader.readLine();
-            while (line != null) {
-                ArrayList<String> headers = new ArrayList<>();
-                Collections.addAll(headers, line.split("\\|"));
-                if (checkingHeader) {
-                    if (headers.contains("native_link")) {
-                        indexNativeLink = headers.indexOf("native_link");
-                    }
-                    if (headers.contains("exception_link")) {
-                        indexExceptionLink = headers.indexOf("exception_link");
-                    }
-                    if (headers.contains("Source Path")) {
-                        indexStageFile = headers.indexOf("Source Path");
-                    }
-                }
-                if (!checkingHeader) {
-                    copyNativeFile(indexStageFile, indexNativeLink, headers);
-                    copyNativeFile(indexStageFile, indexExceptionLink, headers);
-                }
-                checkingHeader = false;
-                line = reader.readLine();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        ProcessingStats.getInstance().taskIsCompressing();
-        ResultCompressor.getInstance().process();
-*/
-    }
-
-    private void copyNativeFile(int indexStageFile, int indexExceptionLink, ArrayList<String> headers) throws IOException {
-/*
-        String newFile;
-        File f;
-        File stage;
-        if (!(newFile = headers.get(indexExceptionLink)).equals("")) {
-            f = new File(tmpFolder + System.getProperty("file.separator") + newFile);
-            stage = new File(headers.get(indexStageFile));
-            f.getParentFile().mkdirs();
-            FileUtils.copyFile(stage, f);
-            ProcessingStats.getInstance().addNativeCopied(stage.length());
-        }
-*/
     }
 }
