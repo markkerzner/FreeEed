a1f72ebe4547f42beb5c9be71a391e2a9896fb8d
Farshid Mehrtash <mehrtash.fm@gmail.com>
2020-06-29 19:07:43 +0430

    removing tika to improve performance

--- freeeed-processing/src/main/java/org/freeeed/Processor/FileProcessor.java
+++ freeeed-processing/src/main/java/org/freeeed/Processor/FileProcessor.java
@@ -28,7 +28,6 @@ import org.freeeed.Entity.ProjectFile;
 import org.freeeed.mr.MetadataWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
@@ -40,31 +39,25 @@ import java.util.List;
 public class FileProcessor implements Runnable {
     private static final Logger LOGGER = LoggerFactory.getLogger(FileProcessor.class);
     private Project project = Project.getActiveProject();
-    private String singleFileName;
     ProjectFile projectFile;
     protected Metadata metadata = new Metadata();
     private final String[] imageExt = new String[]{"jpg", "jpeg", "png", "bmp", "tiff"};
     List<String> imageExtList = Arrays.asList(imageExt);
-    protected Tika tika;
+
     String exceptionMessage = null;
-    private String text = null;
+    protected String text = null;
     private static final String EMPTY = "";
     private static final Tika TIKA = new Tika();
+    TikaInputStream inputStream;
 
     FileProcessor() {
     }
 
-    public String getSingleFileName() {
-        return singleFileName;
-    }
-
     public FileProcessor(ProjectFile projectFile) {
         this.projectFile = projectFile;
-        tika = new Tika();
-        tika.setMaxStringLength(10 * 1024 * 1024);
     }
 
-    private void writeMetadata() {
+    void writeMetadata() {
         MetadataWriter.getInstance().processMap(projectFile, metadata, text);
     }
 
@@ -73,7 +66,7 @@ public class FileProcessor implements Runnable {
         if (project.isDoOcr()) {
 
             try {
-                simpleParse = tika.parseToString(new File(file));
+                simpleParse = TikaAdapter.getInstance().getTika().parseToString(new File(file));
             } catch (IOException | TikaException e) {
                 LOGGER.error("Error during Tika ORC {}", e.getMessage());
             }
@@ -98,13 +91,13 @@ public class FileProcessor implements Runnable {
     public void run() {
         LOGGER.trace("Processing file: {} - {}", projectFile.getFile().getName(), project.getName());
         try {
-            TikaInputStream inputStream = TikaInputStream.get(projectFile.getFile().toPath());
+            inputStream = TikaInputStream.get(projectFile.getFile().toPath());
             if ("pdf".equalsIgnoreCase(projectFile.getExtension()) || imageExtList.contains(projectFile.getExtension())) {
-                tika.parseToString(inputStream, metadata);
+                TikaAdapter.getInstance().getTika().parseToString(inputStream, metadata);
                 text = ocrFile(projectFile.getFile().getPath());
             } else {
                 if (inputStream.available() > 0) {
-                    text = tika.parseToString(inputStream, metadata);
+                    text = TikaAdapter.getInstance().getTika().parseToString(inputStream, metadata);
                 }
             }
             inputStream.close();
